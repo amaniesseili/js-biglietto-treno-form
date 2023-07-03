94ad1ab390383b433b9ed2aa4243de5343e260f7
amaniesseili <esseiliamani@hotmail.com>
2023-07-03 16:51:57 +0200

    biglietto treno form structure

---
+++ main.js
@@ -0,0 +1,51 @@
+const userNameInputElement = document.querySelector("[name='username']")
+const kmInputElement = document.querySelector("[name='km']")
+const ageInputElement = document.querySelector("[name='age']")
+
+const tableTicketElement = document.querySelector(".table")
+
+const btnGenera = document.querySelector(".pulsante-genera");
+const btnAnnula = document.querySelector(".pulsante-annula");
+
+const pricePerKm = 0.21;
+const finalPrice = distance * pricePerKm ;
+
+const discount20 =((distance * pricePerKm) * 20) / 100
+const discount40 =((distance * pricePerKm) * 40) / 100
+
+let discount = 0
+
+
+document.getElementById("username-input").innerHTML = `${userNameInputElement}`;
+document.getElementById("km-input").innerHTML = `${kmInputElement}`;
+document.getElementById("age-input").innerHTML = `${ageInputElement}`;
+
+
+
+if (ageInputElement < 18) {
+    document.getElementById("age-input").innerHTML = ` Complimenti hai uno sconto del 20% `;
+} else {
+if ( userAge >= 18 && userAge < 65){
+    document.getElementById("age-input").innerHTML = ` Ci dispiace, nessun sconto per te!`;
+} else if ( userAge >= 65){
+    document.getElementById("age-input").innerHTML = ` Complimenti hai uno sconto del 40% `;
+}
+}
+
+
+
+btnGenera.addEventListener("click", function(){
+
+    const username = userNameInputElement.value;
+    const km = kmInputElement.value;
+    const age = ageInputElement.value;
+
+    // tableTicketElement.innerHTML =
+
+})
+
+
+
+
+
+
