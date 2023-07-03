ebb03a2572b1f97c3f32903af13ce7d0f821a19a
amaniesseili <esseiliamani@hotmail.com>
2023-07-03 17:10:20 +0200

    distance =kmInputElemet

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ const btnGenera = document.querySelector(".pulsante-genera");
 const btnAnnula = document.querySelector(".pulsante-annula");
 
 const pricePerKm = 0.21;
-const finalPrice = distance * pricePerKm ;
+const finalPrice = kmInputElement * pricePerKm ;
 
-const discount20 =((distance * pricePerKm) * 20) / 100
-const discount40 =((distance * pricePerKm) * 40) / 100
+const discount20 =((kmInputElement * pricePerKm) * 20) / 100
+const discount40 =((kmInputElement* pricePerKm) * 40) / 100
 
 let discount = 0
 
